b4be3d1796033aa032db80dd5e303680a45964ae
Struan Judd <github@scifi.geek.nz>
2021-10-29 17:37:24 +1300

    Add default Powershell profile

---
+++ Powershell_profile.ps1
@@ -0,0 +1,10 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module zLocation
+
+Set-Theme Paradox
+
+Set-Alias d docker
+Set-Alias dc docker-compose
+Set-Alias dn dotnet
+Set-Alias g git
