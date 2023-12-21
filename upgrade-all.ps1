60df15d7ec2fba9e0fccc1ef13b67472c69f077f
Struan Judd <github@scifi.geek.nz>
2023-12-22 07:32:16 +1300

    Update upgrade-all.ps1

--- upgrade-all.ps1
+++ upgrade-all.ps1
@@ -1,11 +1,13 @@
+Write-Host "* Powershell" -ForegroundColor Blue
+$modules = @(Get-InstalledModule | Where-Object { $_.RepositorySourceLocation -ne $_.Repository } | Select-Object -ExpandProperty Name)
+Update-Module $modules
+
+Start-Admin .\windows-updated.ps1
+
 Write-Host "* WSL" -ForegroundColor Blue
 wsa update
 wsau
 wsa autoremove
-
-Write-Host "* Powershell" -ForegroundColor Blue
-$modules = @(Get-InstalledModule | Where-Object { $_.RepositorySourceLocation -ne $_.Repository } | Select-Object -ExpandProperty Name)
-Update-Module $modules
 
 Write-Host "* Dotnet Tools (dntu) " -ForegroundColor Blue
 dn tools-outdated
@@ -15,3 +17,5 @@ wgu
 
 Write-Host "* WSL (wsau)" -ForegroundColor Blue
 wsau
+wsl lsb_release -cd
+wsl do-release-upgrade -c
